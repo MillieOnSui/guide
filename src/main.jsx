b009e66e422c36a2a19950e6178d574a9741e748
MillieOnSui <your-email@example.com>
2026-01-28 09:42:55 -0800

    Update App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LinkXApp from './components/linkx-app-vercel.jsx'
-import './index.css';   
+import LinkXApp from './components/linkx-app-vercel'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LinkXApp />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
